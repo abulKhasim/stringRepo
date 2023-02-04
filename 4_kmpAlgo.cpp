6af53cf0b39e5b99584b3405cb8bec372c2133da
abuKhasim <abulkhasim999@gmail.com>
2023-02-04 15:47:09 +0530

    Final Code

--- 4_kmpAlgo.cpp
+++ 4_kmpAlgo.cpp
@@ -1,56 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool kmpAlgo(string str, string pat){
+// Search Pattern
+vector<int> kmp(string str, string pat)
+{
+    vector<int> pi(pat.size(), 0);
 
-    vector<int> lps(pat.size(),0);
+    for (int i = 1; i < pat.size(); i++)
+    {
+        int j = pi[i - 1];
 
-    for(int i=1; i<pat.size(); i++){
-        int j = lps[i-1];
-
-        while(j>0 && pat[j] != pat[i]){
-            j = lps[j-1];
+        while (j > 0 && pat[j] != pat[i])
+        {
+            j = pi[j - 1];
         }
 
-        if(pat[j] == pat[i]) j++;
+        if (pat[j] == pat[i])
+        {
+            j++;
+        }
 
-        lps[i] = j;
+        pi[i] = j;
     }
 
     int n = str.size();
     int m = pat.size();
-    int i=0;
-    int j=0;
+    int i = 0;
+    int j = 0;
     vector<int> ans;
 
-    while(i<n){
-        if(pat[j] == str[i]){
+    while (i < n)
+    {
+        if (pat[j] == str[i])
+        {
             i++;
             j++;
         }
 
-        if(j==m){
-            return true;
-            // ans.push_back(i-(m-1));
+        if (j == m)
+        {
+            ans.push_back(i - (m - 1));
+            j = pi[j - 1];
         }
-        else if(pat[j] != str[i]){
-            if(j==0) i++;
-            j = lps[j-1];
+        else if (pat[j] != str[i])
+        {
+            if (j == 0)
+                i++;
+            j = pi[j - 1];
         }
     }
 
-    return false;
-
+    return ans;
 }
 
-int main(){
-
+int main()
+{
     string str = "batmanandrobinarebat";
-    string pat = "nare";
+    string pat = "bat";
 
-    bool ans  = kmpAlgo(str, pat);
-    cout << ans << endl;
+    vector<int> ans = kmp(str, pat);
 
+    for (auto i : ans)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
 
     return 0;
 }
